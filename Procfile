f6e78a5de954a43cf7ade6cb687e7846085cea21
galletti <galletti@bu.edu>
2017-10-23 20:52:12 -0400

    Updating: minor update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn webapp:app
+web: python3 webapp.py
--- webapp.py
+++ webapp.py
@@ -832,4 +832,6 @@ def recommendations():
 
 if __name__=='__main__':
     app.secret_key = os.urandom(100)
-    app.run(debug=True)
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
