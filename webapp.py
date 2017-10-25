8cd7fe689b6999863fd6a6f3a839910b26273a05
galletti <galletti@bu.edu>
2017-10-25 10:27:08 -0400

    Updating: fixing minor issues

--- webapp.py
+++ webapp.py
@@ -911,8 +911,7 @@ def search():
 
             pic_and_data = []
             for item in cursor:
-                img = ''.join(list(str(item[1]))[2:-1])
-                pic_and_data.append([int(item[0]), img])
+                pic_and_data.append([int(item[0]), item[1]])
 
             #preserving the order
 
